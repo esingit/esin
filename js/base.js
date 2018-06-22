c8b7c248cf0b79b8b108467061142671b185e6a9
esinhtml <291626442@qq.com>
2018-06-23 07:26:28 +0800

    123

--- js/base.js
+++ js/base.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
+    var timer = null;
+    var drop_down_con = document.getElementsByClassName('.drop_down_con')
+
     $(".drop_down").mouseover(function(){
-        $(".drop_down img").attr("src","image/high.gif");
+        $(".drop_down img").attr("src","image/tutu.gif");
         $(".drop_down span").addClass("drop_down_span");
         $(".drop_down").addClass("drop_down_bg");
-        // $(".drop_down>div").addClass("drop_down_con_show");
-        $(".drop_down>div").slideDown();
-    })
-    $(".drop_down").mouseout(function(){
-        $(".drop_down img").attr("src","image/WOW.gif");
-        $(".drop_down span").removeClass("drop_down_span");
-        $(".drop_down").removeClass("drop_down_bg");
-        $(".drop_down>div").removeClass("drop_down_con_show");
-    })
-    $("body").mouseover(function(e){
-        if($(e.target).closest(".drop_down").length==0){
-            $(".drop_down>div").slideUp();
-        }
-    })
+        $(".drop_down>div").addClass("drop_down_con_show");
+        // $(".drop_down>div").slideDown();
+    });
+        $(".drop_down").mouseout(function(){
+           
+            $(".drop_down img").attr("src","image/WOW.gif");
+            $(".drop_down span").removeClass("drop_down_span");
+            $(".drop_down").removeClass("drop_down_bg");
+            $(".drop_down>div").removeClass("drop_down_con_show");
+           
+        });
+        $("body").mouseover(function(e){
+            if($(e.target).closest(".drop_down").length==0){
+                $(".drop_down>div").removeClass("drop_down_con_show");
+                // $(".drop_down>div").slideUp();
+            };
+        });
 });
