8dc1f4de2a63f25e0c962898c04effda73a198bb
esinhtml <291626442@qq.com>
2018-06-22 19:56:14 +0800

    123

---
+++ js/my.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".drop_down").mouseover(function(){
+        $(".drop_down img").attr("src","image/high.gif");
+        $(".drop_down span").addClass("drop_down_span");
+        $(".drop_down").addClass("drop_down_bg");
+        // $(".drop_down>div").addClass("drop_down_con_show");
+        $(".drop_down>div").slideDown();
+    })
+    $(".drop_down").mouseout(function(){
+        $(".drop_down img").attr("src","image/WOW.gif");
+        $(".drop_down span").removeClass("drop_down_span");
+        $(".drop_down").removeClass("drop_down_bg");
+        $(".drop_down>div").removeClass("drop_down_con_show");
+    })
+    $("body").mouseover(function(e){
+        if($(e.target).closest(".drop_down").length==0){
+            $(".drop_down>div").slideUp();
+        }
+    })
+});
