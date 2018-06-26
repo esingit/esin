36ebd5a597dcd59981d46d4309c64a5effe4d632
esinhtml <291626442@qq.com>
2018-06-26 11:14:21 +0800

    123

--- js/base.js
+++ js/base.js
@@ -1,5 +1,16 @@
   
 $(document).ready(function(){
+    $(function () {
+        $('html').animate( {scrollTop: 0}, 500);
+        $("a").click(function () {
+
+            $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top -20+ "px"}, 500);
+        
+            return false;//不要这句会有点卡顿
+        
+        });
+    });
+
     $(function(){
         //获取要定位元素距离浏览器顶部的距离
         var navH = $(".menu_bg").offset().top;
@@ -10,11 +21,15 @@ $(document).ready(function(){
             //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
             if(scroH>=navH){
             $(".menu_bg").css({"position":"fixed","top":"0px"});
+            $(".side").css({"position":"fixed","top":"180px"});
             $(".content_bg").css("margin","44px auto 0 auto");
+            $(".back").css({"right":"50px","bottom":"50px"});
             }
             else if(scroH<navH){
             $(".menu_bg").css("position","relative");
             $(".content_bg").css("margin","0 auto");
+            $(".side").css({"position":"absolute","top":"280px"});
+            $(".back").css({"right":"1150px","bottom":"1150px"});
             };
         });
     });
@@ -28,41 +43,44 @@ $(document).ready(function(){
             $(".content_middle_con").css("height","0");
         });
     });
-       
+
     $(function(){
-      
-        $(".pic1_li").mouseover(function(){
-            var index=$(this).index();
-            i=index;
-        $(".pic2").eq(index).css("display","block");
-        
-        })
-        $('.pic1_li').mouseout(function(){
-        var i=$(this).index();
-            $(".pic2").eq(i).css("display","none");
-            
-        })
+        $(".pic1_li,.pic0").hover(function(){  
+            $(".pic0").css({"display":"block"});
+        },function(){  
+            $(".pic0").css({"display":"none"});
+        });
+
+
+        $(".pic2_li,.pic1").hover(function(){  
+            $(".pic1").css({"display":"block"});
+        },function(){  
+            $(".pic1").css({"display":"none"});
+        });
+
+
+        $(".pic3_li,.pic2").hover(function(){  
+            $(".pic2").css({"display":"block"});
+        },function(){  
+            $(".pic2").css({"display":"none"});
+        });
     });
-    // $(".pic1_li,.pic1").hover(function(){  
-    //     $(".pic1").css({"display":"block"});
-    // },function(){  
-    //     $(".pic1").css({"display":"none"});
-    // });
-
-
-    // $(".pic2_li,.pic2").hover(function(){  
-    //     $(".pic2").css({"display":"block"});
-    // },function(){  
-    //     $(".pic2").css({"display":"none"});
-    // });
-
-
-    // $(".pic3_li,.pic3").hover(function(){  
-    //     $(".pic3").css({"display":"block"});
-    // },function(){  
-    //     $(".pic3").css({"display":"none"});
-    // });
-
-
+    
+    $(function(){  
+        $('a[href*=#],area[href*=#]').click(function() {
+          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+              var targetOffset = $target.offset().top;
+              $('html,body').animate({
+                scrollTop: targetOffset
+              },
+              1000);
+              return false;
+            }
+          }
+        });
+      })
 });
 
